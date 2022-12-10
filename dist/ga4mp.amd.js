22663935af3151bd25ab94e1b639945516a7b5f5
David Vallejo <thyngster@gmail.com>
2022-12-10 03:05:58 +0100

    0.0.1 updates dist

--- dist/ga4mp.amd.js
+++ dist/ga4mp.amd.js
@@ -1,6 +1,6 @@
 /*!
 * 
-*   @analytics-debugger/ga4mp 0.0.2
+*   @analytics-debugger/ga4mp 0.0.1
 *   https://github.com/analytics-debugger/ga4mp
 *
 *   Copyright (c) David Vallejo (https://www.thyngster.com).
